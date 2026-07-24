e4df1d0eaa5890c07cf8b1200daecfa9f72e7204
marenecol <andreacollab28@gmail.com>
2026-07-24 18:48:17 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -18,15 +18,53 @@ window.PRODUCTS = [
             "assets/images/maria1.JPG",
             "assets/images/maria2.JPG"
         ],
-        // Definimos los colores disponibles y un color seleccionado por defecto
         colors: ["Blanco", "Dorado"],
         selectedColor: "Blanco",
-        description: "", // La dejamos vacía porque me pediste quitar el texto largo
+        description: "",
         features: [
             "Referencia: MARIA",
             "Tejido artesanalmente flor por flor",
             "Elaborado con perlas delicadas y mostacillas seleccionadas",
             "Incluye dije colgante floral ajustable"
+        ]
+    },
+    {
+        id: "cinturon-AURA",
+        name: "Cinturón Murano & Perlas",
+        price: 40000,
+        category: "cinturones",
+        categoryLabel: "Cinturones",
+        image: "assets/images/cinturon-murano1.jpg", 
+        images: [
+            "assets/images/cinturon-murano1.jpg"
+        ],
+        colors: ["otro", "Dorado", "Plateado"],
+        selectedColor: "Multicolor",
+        description: "",
+        features: [
+            "Referencia: CINTURÓN MAR",
+            "Tejido artesanal a mano",
+            "Elaborado con muranos, perlas y mostacillas",
+            "Diseño sutil, elegante y ajustable"
+        ]
+    },
+    {
+        id: "collar-MAR",
+        name: "Collar MAR",
+        price: 30000,
+        category: "collares",
+        categoryLabel: "Collares",
+        image: "assets/images/collar-mostacillas1.jpg", 
+        images: [
+            "assets/images/collar-mostacillas1.jpg"
+        ],
+        colors: ["Blanco", "Dorado", "Rosa"],
+        selectedColor: "Blanco",
+        description: "",
+        features: [
+            "Referencia: COLLAR MAR",
+            "Tejido artesanal a mano",
+            "Elaborado con mostacillas",
         ]
     }
 ];
